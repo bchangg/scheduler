f1038a379bfc8efdc883fbaef4b88079c99ecb91
Brian Chang <changbhc@gmail.com>
2019-08-05 20:24:51 -0700

    added Header, Empty, index, styles.scss

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header(props) {
+  return (
+    <main class="appointment__add">
+      <img
+        class="appointment__add-button"
+        src="images/add.png"
+        alt="Add"
+        onClick={props.onAdd()}
+      />
+    </main>
+  )
+};
--- stories/index.js
+++ stories/index.js
@@ -10,7 +10,15 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+
+import Confirm from "components/Appointment/Confirm";
+import Empty from "components/Appointment/Empty";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
+import Header from "components/Appointment/Header";
 import Appointment from "components/Appointment";
+import Show from "components/Appointment/Show";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -134,4 +142,6 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm" />);
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={()=>action("emptyClick")} />);
